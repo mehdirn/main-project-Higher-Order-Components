4b95882edc9b8394e61199ef98709d66e6ddba29
mehdirn <mehdideris0938@gmail.com>
2023-06-19 02:45:00 +0430

    end HOC exercise

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import questions from './data';
+import Question from './Components/Question';
 
 function App() {
+
+  const [infos , setInfos] = useState(questions)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className='container'>
+        <h3>questions and answers about login</h3>
+        <section className='info'>
+          {infos.map(info => (
+            <Question key={info.id} {...info} />
+          ))}
+        </section>
+      </div>
+    </main>
   );
 }
 
